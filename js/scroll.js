9e8f1382571800c112f0242cb1332b384461bb4f
Marcelo Moura de Lima <marcelogoodtrip@gmail.com>
2022-04-27 17:36:14 -0300

    new 27/04/2022 - 17:33

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+
+console.log(menuItems);
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    });
+}
